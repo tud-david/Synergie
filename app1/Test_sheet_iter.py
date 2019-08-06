34c47d7caf1c1e47ab1d7550d058fe306932b971
Maximilian Sporleder <Sporleder_M@PTW.Maschinenbau.TU-Darmstadt.de>
2019-08-06 14:42:06 +0200

    components search

--- app1/Test_sheet_iter.py
+++ app1/Test_sheet_iter.py
@@ -1,5 +1,9 @@
+from utilities import component_search
 
+xml_file = 'modelDescription.xml'
 
+ps, paths, components = component_search(xml_file)
+print(paths)
 
 def dict_rec_ctt_fun(dictionary, records_paths, ctt_paths):
     ## dictionary to store the records and the ctt's with their paths as keys and their value as value
